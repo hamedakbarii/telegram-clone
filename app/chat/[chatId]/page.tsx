79f312cdccef1ce72390ca54afdfdd1902141975
Amir Rahemi <amirrahemi01@gmail.com>
2025-07-28 14:18:32 +0330

    Dark Light

---
+++ app/chat/[chatId]/page.tsx
@@ -0,0 +1,10 @@
+// Path: app/chat/[chatId]/page.tsx
+import React from 'react'
+
+type Props = {}
+
+export default function page({}: Props) {
+  return (
+    <div>page</div>
+  )
+}
--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -18,10 +18,15 @@ export function ThemeToggle() {
     toggleTheme();
   };
 
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.stopPropagation(); // Prevent bubbling to parent
+    toggleTheme();
+  };
+
   return (
-    <div onClick={handleClick} className="flex items-center px-4 py-2 text-sm font-medium transition duration-300 w-full text-left hover:bg-[#151515] dark:hover:bg-[#151515] cursor-pointer">
+    <div onClick={handleClick} className="flex items-center justify-between px-4 py-2 text-sm font-medium transition duration-300 w-full text-left hover:bg-[#151515] dark:hover:bg-[#151515] cursor-pointer">
       <button
-        className="rounded-full hover:bg-gray-200 dark:hover:bg-gray-700 transition-colors"
+        className="flex rounded-full"
         aria-label={`Toggle ${theme === "light" ? "dark" : "light"} mode`}
       >
         {theme === "light" ? (
@@ -29,8 +34,18 @@ export function ThemeToggle() {
         ) : (
           <IoSunnyOutline size={20} className="mr-2 w-5 h-5" />
         )}
+        Night Mode
       </button>
+      {/* Toggle Button */}
+      <label className="inline-flex items-center cursor-pointer" onClick={(e) => e.stopPropagation()}> 
+        <input 
+          type="checkbox" 
+          checked={theme === "dark"} 
+          onChange={handleInputChange}
+          className="sr-only peer"
+        />
+        <div className="relative w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-transparent dark:peer-focus:ring-transparent rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-[#8774E1] dark:peer-checked:bg-[#8774E1]"></div>
+      </label>
     </div>
-
   );
 }
