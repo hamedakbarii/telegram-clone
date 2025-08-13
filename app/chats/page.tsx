cdf02f401b5c5c84bbb871755a61d37e6f14695b
hamedakbari <hamedakbariwork@gmail.com>
2025-08-13 20:12:00 +0330

    redirect bug fixed in chats page

--- app/chats/page.tsx
+++ app/chats/page.tsx
@@ -1,34 +1,9 @@
 // Path: app/chats/page.tsx
 "use client";
 
-import React, { useEffect, useState } from "react";
-import { useStore } from "@/store/useAuthStore";
-import { useRouter } from "next/navigation";
+import React from "react";
 import MainContent from "@/components/MainContent";
 
 export default function ChatListPage() {
-  const [isLoading, setIsLoading] = useState(true);
-  
-  const user = useStore((state) => state.user);
-  const router = useRouter();
-
-  useEffect(() => {
-    // Check if user is authenticated using the Zustand store
-    if (!user) {
-      router.push("/login");
-    } else {
-      setIsLoading(false);
-    }
-  }, [user, router]);
-
-  // Show loading state while checking authentication
-  if (isLoading) {
-    return (
-      <div className="flex h-screen items-center justify-center bg-[#FEFEFF] dark:bg-[#202021]">
-        <div className="text-gray-500 dark:text-gray-400">Loading...</div>
-      </div>
-    );
-  }
-
   return <MainContent showUserProfile={false} />;
 }
